7c3b7cca1ec41f23e5d0b5a292c787d9acc41da3
Ktimpka <ktimpka@hotmail.com>
2018-11-19 15:56:16 +0200

    Modbus RTU add

---
+++ Private_Driver/sw/inc/modbus_rtu.h
@@ -0,0 +1,41 @@
+/*
+ * modbus_rtu.h
+ *
+ *  Created on: 19. nov 2018
+ *      Author: Toftan2-elekter
+ */
+
+#ifndef SW_INC_MODBUS_RTU_H_
+#define SW_INC_MODBUS_RTU_H_
+
+/*
+ * Includes
+ * */
+#include "FreeRTOS.h"
+#include "semphr.h"
+
+#include "stdint.h"
+#include "hw_usart3.h"
+
+/*
+ * Defines, macros
+ * */
+
+/*
+ * GLOBAL
+ * */
+
+/*
+ * Functions
+ * */
+
+void mb_rtu_init();
+/*
+ * NVIC functions
+ * */
+
+/*
+ * Threads
+ * */
+
+#endif /* SW_INC_MODBUS_RTU_H_ */
---
+++ Private_Driver/sw/src/modbus_rtu.c
@@ -0,0 +1,14 @@
+/*
+ * modbus_rtu.c
+ *
+ *  Created on: 19. nov 2018
+ *      Author: Toftan2-elekter
+ */
+
+/*
+ * Initialize hardware and buffer for Modbus RTU.
+ * */
+void mb_rtu_init()
+{
+
+}
